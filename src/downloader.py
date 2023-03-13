6d4577f43d75616b0d380224668e4f0554b945ba
EvanHsieh0415 <evan940415@gmail.com>
2023-03-13 11:00:20 +0800

    fix: bugs

--- src/downloader.py
+++ src/downloader.py
@@ -35,26 +35,23 @@ class GuildDownloader:
     image_format_mapping: tuple[str] | dict[int, str]
     format_key: str
     assets_url: str
-    filename_type: str = "name"
 
     def __init__(
         self,
         guild_id: int,
         token: str,
+        filename_type: str
     ) -> None:
-        self.save_path = create_dirs(f"./data/{self.object_type}/{guild_id}")
-        self.object_list = get_guild_object(guild_id, token)[self.object_type]
-        logging.info(f"Get {object} list")
-        save_information(
-            f"{self.save_path}/{self.object_type}_information", self.object_list
-        )
+        save_path = create_dirs(f"./data/{self.object_type}/{guild_id}")
+        object_list = get_guild_object(guild_id, token)[self.object_type]
+        logging.info(f"Get {self.object_type} list")
+        save_information(f"{save_path}/{self.object_type}_information", object_list)
 
-    def run(self):
         logging.info(f"Download {self.object_type} task start...")
-        for object in self.object_list:
-            filename = f"{object[self.filename_type]}.{self.image_format_mapping[object[self.format_key]]}"
+        for object in object_list:
+            filename = f"{object[filename_type]}.{self.image_format_mapping[object[self.format_key]]}"
             image_bytes = requests.get(self.assets_url.format(object["id"])).content
-            with open(file=f"{self.save_path}/{filename}", mode="wb") as file_ref:
+            with open(file=f"{save_path}/{filename}", mode="wb") as file_ref:
                 file_ref.write(image_bytes)
             logging.info(f"Save file success, {filename = }")
         logging.info(f"Download {self.object_type} task done...")
