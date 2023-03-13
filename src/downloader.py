52f17a908c0dc19ce6ff17a6d53715e0dd7953ee
EvanHsieh0415 <evan940415@gmail.com>
2023-03-13 10:52:13 +0800

    refator: OOP's style

--- src/downloader.py
+++ src/downloader.py
@@ -1,9 +1,9 @@
 import logging
 import os
-import typing
-import requests
 import json
 
+import requests
+import pretty_errors
 
 logging.basicConfig(level=logging.INFO)
 
@@ -11,45 +11,67 @@ API_VERSION = 10
 API_BASE_URL = f"https://discord.com/api/v{API_VERSION}"
 
 
-def download_emoji(
-    guild_id: int,
-    token: str,
-    save_path: str = "./data/emojis",
-    filename_type: str = "name",
-) -> None:
-    # Create dirs
-    save_path = f"{save_path}/{guild_id}"
-    if not os.path.isdir(save_path):
-        os.makedirs(save_path)
-    logging.info(f"Create dirs: \"{save_path}\"")
+def create_dirs(dirs: str):
+    if not os.path.isdir(dirs):
+        os.makedirs(dirs)
+    logging.info(f'Create dirs: "{dirs}"')
+    return dirs
 
-    # Get emoji list
-    emoji_list: list[dict] = requests.get(
+
+def get_guild_object(guild_id: int, token: str) -> dict:
+    return requests.get(
         url=f"{API_BASE_URL}/guilds/{guild_id}", headers={"Authorization": token}
-    ).json()["emojis"]
-    logging.info("Get emoji list")
-
-    # Save emoji information
-    with open(
-        file=f"{save_path}/emoji_info.json", mode="w", encoding="utf-8"
-    ) as file_ref:
-        json.dump(obj=emoji_list, fp=file_ref, indent=2)
-    logging.info(f"Save emoji file on \"{save_path}/emoji_info.json\"")
-
-    # Data mapping
-    image_format_mapping = ["png", "apng"]
-
-    # Save files
-    logging.info("Download emoji task start...")
-    for emoji_object in emoji_list:
-        filename = f"{emoji_object[filename_type]}.{image_format_mapping[emoji_object['animated']]}"
-        image_bytes = requests.get(
-            f"https://cdn.discordapp.com/emojis/{emoji_object['id']}?size=4096&quality=lossless"
-        ).content
-        with open(file=f"{save_path}/{filename}", mode="wb") as file_ref:
-            file_ref.write(image_bytes)
-        logging.info(f"Save file success, {filename = }")
-    logging.info("Download emoji task done...")
+    ).json()
 
 
-__all__ = ["download_emoji"]
+def save_information(file_path: str, information: dict | list):
+    with open(file=f"{file_path}.json", mode="w", encoding="utf-8") as file_ref:
+        json.dump(obj=information, fp=file_ref, indent=2)
+    logging.info(f'Save emoji file on "{file_path}/emoji_info.json"')
+
+
+class GuildDownloader:
+    object_type: str
+    image_format_mapping: tuple[str] | dict[int, str]
+    format_key: str
+    assets_url: str
+    filename_type: str = "name"
+
+    def __init__(
+        self,
+        guild_id: int,
+        token: str,
+    ) -> None:
+        self.save_path = create_dirs(f"./data/{self.object_type}/{guild_id}")
+        self.object_list = get_guild_object(guild_id, token)[self.object_type]
+        logging.info(f"Get {object} list")
+        save_information(
+            f"{self.save_path}/{self.object_type}_information", self.object_list
+        )
+
+    def run(self):
+        logging.info(f"Download {self.object_type} task start...")
+        for object in self.object_list:
+            filename = f"{object[self.filename_type]}.{self.image_format_mapping[object[self.format_key]]}"
+            image_bytes = requests.get(self.assets_url.format(object["id"])).content
+            with open(file=f"{self.save_path}/{filename}", mode="wb") as file_ref:
+                file_ref.write(image_bytes)
+            logging.info(f"Save file success, {filename = }")
+        logging.info(f"Download {self.object_type} task done...")
+
+
+class EmojiDownloader(GuildDownloader):
+    object_type = "emojis"
+    image_format_mapping = "png", "apng"
+    format_key = "animated"
+    assets_url = "https://cdn.discordapp.com/emojis/{}?size=4096&quality=lossless"
+
+
+class StickersDownloader(GuildDownloader):
+    object_type = "stickers"
+    image_format_mapping = ("", "png", "apng", "json", "gif")
+    format_key = "format_type"
+    assets_url = "https://media.discordapp.net/stickers/{}?size=4096"
+
+
+__all__ = ["EmojiDownloader", "StickersDownloader"]
